18be0431aee1ca26bf3046066cea99b41aae7351
Deepak Dey <deepakdey412@gmail.com>
2025-08-15 00:09:07 +0530

    Upload Day 6 SpringSecurity practice folder

---
+++ Day06-SpringSecurity-custom-configuration/SpringSecurityCustomConfig/src/test/java/com/SpringSecurityCustomConfig/SpringSecurityCustomConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurityCustomConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCustomConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
